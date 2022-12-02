504887160fc1663fccce8cf08cf359a620d4fd9e
tonymg1 <tonymartingaldeano@gmail.com>
2022-12-02 17:50:24 +0100

    wip

--- vite-project/src/pages/ProfilePage.jsx
+++ vite-project/src/pages/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const ProfilePage = () => {
-  return <div>profilePage</div>;
+export const ProfilePage = () => {
+  return <h1>profilePage</h1>;
 };
 
-export default ProfilePage;
+
--- vite-project/src/router/Router.jsx
+++ vite-project/src/router/Router.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, RegisterPage, AuthHomePage } from "../auth/pages/index.js";
+import { LoginPage, RegisterPage, AuthHomePage} from "../auth/pages/index.js";
+import {ProfilePage} from '../pages/ProfilePage'
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AuthHomePage />} />
+          <Route path="/" element={<ProfilePage/>} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
