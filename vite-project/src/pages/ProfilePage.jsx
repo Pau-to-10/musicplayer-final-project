377d9049050c2eba02f83613ed3d758f04b53d15
tonymg1 <tonymartingaldeano@gmail.com>
2022-12-05 12:05:52 +0100

    wip

--- vite-project/src/pages/ProfilePage.jsx
+++ vite-project/src/pages/ProfilePage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import {Frame} from '../ui/probando'
+import {Frame} from '../ui/probando';
+import {Button, Input, Input2, Footer, IconFooter} from '../ui/Buttons.style'
+import {GridRegisterLogin } from '../ui/Gridstyle'
+import {Img, Img2} from '../ui/Img'
+
 
 export const ProfilePage = () => {
   return (
-    <Frame>
-      <img src="./assets/img/face.png" alt="" />
-    </Frame>
+    
+      <GridRegisterLogin>
+        
+        <Input placeholder='user@mail.com'/>
+        <Input2 placeholder='Username'/>
+      
+        <Button>Logout</Button>
+        <Footer><IconFooter></IconFooter></Footer>
+
+      </GridRegisterLogin>
+      
+    
   )
 }
 
--- vite-project/src/ui/Buttons.style.js
+++ vite-project/src/ui/Buttons.style.js
@@ -25,7 +25,7 @@ width: 334px;
 height: 80px;
 
 left: 53px;
-top: 113px;
+top: 260px;
 padding: 10px;
 //background: #D9D9D9;
 background-color: white;
@@ -33,3 +33,47 @@ opacity: 0.14;
 border: 1px solid white;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 border-radius: 30px;`
+
+
+export const Input2 = styled.input`
+/* Rectangle 25 */
+
+
+box-sizing: border-box;
+position: absolute;
+width: 334px;
+height: 80px;
+
+left: 53px;
+top: 360px;
+padding: 10px;
+//background: #D9D9D9;
+background-color: white;
+opacity: 0.14;
+border: 1px solid white;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;`
+
+
+export const Footer = styled.footer`
+position: absolute;
+width: 428px;
+height: 73px;
+left: 0px;
+background-color: #2F2C79;
+top: 853px;`
+
+
+export const IconFooter = styled.div`
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 0px;
+gap: 63px;
+
+position: absolute;
+width: 330.33px;
+height: 28px;
+left: 48.29px;
+top: 876px;
+`
