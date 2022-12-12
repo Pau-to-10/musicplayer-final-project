90913903f70000e999ff183a339b6021b6879f29
Your Name <elizabeth.ramos101@hotmail.com>
2022-12-12 09:38:00 +0000

    register

--- vite-project/src/components/RegisterForm.jsx
+++ vite-project/src/components/RegisterForm.jsx
@@ -4,73 +4,83 @@ import { Button, InputSmall } from "../ui";
 import { Form } from "../ui/Form";
 
 export const RegisterForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data.email);
-    validate(data.email);
-  };
 
-  console.log(watch("name")); // watch input value by passing the name of it
+
+
+
+
+  
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm();
+  //const onSubmit = (data) => {
+    //console.log(data.email);
+    //validate(data.email);
+  // };
+
+  //console.log(watch("name")); // watch input value by passing the name of it
   /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
 
-  const userDB = "hola@gmail.com";
+  //const userDB = "hola@gmail.com";
 
-  function validate(email, emailDb) {
-    //request to db with a get user
-    if (email === emailDb) {
-      console.log("This emaili s already registered, recover your password");
-    } else {
-      //if it not registered call post function
-    }
-  }
+  // function validate(email, emailDb) {
+  //   //request to db with a get user
+  //   if (email === emailDb) {
+  //     console.log("This emaili s already registered, recover your password");
+  //   } else {
+  //     //if it not registered call post function
+  //   }
+  // }
   return (
     <>
       <Form onSubmit={handleSubmit(onSubmit)}>
         {/* register your input into the hook by invoking the "register" function */}
-        <InputSmall
+        {/* <InputSmall
           {...register("name", { required: true })}
           placeholder="Name"
-        />
+        /> */}
+        <InputSmall type="text" name="userName" placeholder="UserName" />
 
         {/* include validation with required or other standard HTML validation rules */}
-        <InputSmall
+        {/* <InputSmall
           {...register("first_name", { required: true })}
           placeholder="First name"
-        />
+        /> */}
+        <InputSmall type="text" name="first_name" placeholder="First Name" />
+        <InputSmall type="text" name="last_name" placeholder="Last Name" />
 
-        {errors.exampleRequired && <span>This field is required</span>}
+        {/* {errors.exampleRequired && <span>This field is required</span>} */}
 
-        <InputSmall
+        {/* <InputSmall
           {...register("email", { required: true })}
           placeholder="Email"
         />
-        {errors.exampleRequired && <span>This field is required</span>}
+        {errors.exampleRequired && <span>This field is required</span>} */}
+        <InputSmall type="text" name="email" placeholder="Email" />
 
-        <InputSmall
+        {/* <InputSmall
           {...register("username", { required: true })}
           placeholder="Username"
         />
+        {errors.exampleRequired && <span>This field is required</span>} */}
+        <InputSmall type="text" name="address" placeholder="Address" />
 
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <InputSmall
+        {/* <InputSmall
           {...register("password", { required: true })}
           placeholder="Password"
         />
+        {errors.exampleRequired && <span>This field is required</span>} */}
+        <InputSmall type="text" name="password" placeholder="Password" />
 
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <InputSmall
+        {/* <InputSmall
           {...register("confirmPassword", { required: true })}
           placeholder="Confirm Password"
         />
-
-        {errors.exampleRequired && <span>This field is required</span>}
+        {errors.exampleRequired && <span>This field is required</span>} */}
+        <InputSmall type="text" name="confirmPassword" placeholder="Confirm your password" />
 
         <Button type="submit"> Save</Button>
       </Form>
